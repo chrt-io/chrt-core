f425a5407cd8e8706daa40f06f59364dc0e95120
carlo <littleark@gmail.com>
2021-06-08 19:33:16 +0100

    data to transform plain array into a x/y array

--- src/util/data.js
+++ src/util/data.js
@@ -1,4 +1,4 @@
-export default function (data, accessor) {
+export default function(data, accessor) {
   // console.log('---------------> data', data, accessor, this);
   if (!data) {
     return this._data;
@@ -9,17 +9,28 @@ export default function (data, accessor) {
 
   // // console.log('DATA', this, this._data, data);
 
-
+  const defaultAccessorFunction = (d, i) => {
+    if (typeof d !== 'object') {
+      return {
+        x: i,
+        y: d
+      };
+    }
+    return {
+      x: d.x ?? i,
+      y: d.y ?? d
+    };
+  };
 
   // define accessor function to map values
   const accessorFunction = accessor || this._accessor;
-  this._accessor = accessorFunction;
-  this._data = accessorFunction ? data.map((d, i, arr) => {
-    if(d instanceof Object) {
-      return Object.assign({}, d, accessorFunction(d, i, arr));
+  this._accessor = accessorFunction ?? defaultAccessorFunction;
+  this._data = data.map((d, i, arr) => {
+    if (d instanceof Object) {
+      return Object.assign({}, d, this._accessor(d, i, arr));
     }
-    return accessorFunction(d, i, arr);
-  }) : data;
+    return this._accessor(d, i, arr);
+  });
 
   // console.log('DATA', this._data)
 
