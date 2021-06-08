0722711aae4db3db3a83e7c5da05a7dd4ec9c1c5
carlo <littleark@gmail.com>
2021-06-08 19:33:32 +0100

    help to return a list instead of console.log

--- src/util/help.js
+++ src/util/help.js
@@ -1,29 +1,30 @@
 function args(func) {
-    return (func + '')
-      .replace(/[/][/].*$/mg,'') // strip single-line comments
-      .replace(/\s+/g, '') // strip white space
-      .replace(/[/][*][^/*]*[*][/]/g, '') // strip multi-line comments
-      .split('){', 1)[0].replace(/^[^(]*[(]/, '') // extract the parameters
-      .replace(/=[^,]+/g, '') // strip any ES6 defaults
-      .split(',').filter(Boolean); // split & filter [""]
+  return (func + '')
+    .replace(/[/][/].*$/gm, '') // strip single-line comments
+    .replace(/\s+/g, '') // strip white space
+    .replace(/[/][*][^/*]*[*][/]/g, '') // strip multi-line comments
+    .split('){', 1)[0]
+    .replace(/^[^(]*[(]/, '') // extract the parameters
+    .replace(/=[^,]+/g, '') // strip any ES6 defaults
+    .split(',')
+    .filter(Boolean); // split & filter [""]
 }
 
 function getMethods(obj) {
-  let properties = new Set()
-  let currentObj = obj
+  let properties = new Set();
+  let currentObj = obj;
   do {
-    Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
-  } while ((currentObj = Object.getPrototypeOf(currentObj)))
-  return [...properties.entries()].filter(item => typeof obj[item[0]] === 'function')
+    Object.getOwnPropertyNames(currentObj).map(item => properties.add(item));
+  } while ((currentObj = Object.getPrototypeOf(currentObj)));
+  return [...properties.entries()].filter(
+    item => typeof obj[item[0]] === 'function'
+  );
 }
 
-export default function (obj) {
-  console.log('HELP')
+export default function(obj) {
   const funcs = getMethods(obj);
-  console.log(funcs)
-  funcs.forEach(func => {
-    const functionArgs = args(obj[func[0]]);
-    console.log(func[0], functionArgs);
-  })
-  console.log('####')
+  return funcs.map(func => ({
+    name: func[0],
+    parameters: args(obj[func[0]])
+  }));
 }
